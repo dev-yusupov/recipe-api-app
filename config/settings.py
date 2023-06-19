0bc9029e7534fa2d0083ba6d610f2551ae93d6c5
Bobur Yusupov <boburinfo2006@gmail.com>
2023-06-19 13:24:31 +0500

    Configured Postgres

--- config/settings.py
+++ config/settings.py
@@ -73,13 +73,22 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": 'django.db.backends.postgresql',
+        "HOST": 'localhost',
+        "NAME": "recipe-api-app",
+        "USER": "postgres",
+        "PASSWORD": "20062007"
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
