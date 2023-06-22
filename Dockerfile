bd91d7d53d8c41a82e9d44d461900988d99c9ac0
Bobur Yusupov <boburinfo2006@gmail.com>
2023-06-22 09:23:34 +0500

    Configured user test

--- Dockerfile
+++ Dockerfile
@@ -3,26 +3,7 @@ LABEL maintainer = "anur.tj"
 
 ENV PYTHONUNBUFFERED 1
 
-# COPY ./requirements.txt /tmp/requirements.txt
-# COPY . /config
-
-# WORKDIR /app
-# EXPOSE 8000
-
-# RUN python -m venv /py && \
-#     /py/bin/pip install --upgrade pip && \
-#     /py/bin/pip install -r /tmp/requirements.txt && \
-#     rm -rf /tmp && \
-#     adduser \
-#         --disabled-password \
-#         --no-create-home \
-#         django-user
-
-# ENV PATH="/py/bin:$PATH"
-
-# USER django-user
-
-COPY ./requirements.txt /tmp/requirements.txt
+COPY ./requirements.txt /requirements.txt
 COPY . /app
 WORKDIR /app
 EXPOSE 8000
--- user/test/test_user_api.py
+++ user/test/test_user_api.py
@@ -45,7 +45,7 @@ class PublicUserApiTests(TestCase):
         }
 
         create_user(**payload)
-        response = self.client.get(CREATE_USER_URL, payload)
+        response = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
     
@@ -56,7 +56,7 @@ class PublicUserApiTests(TestCase):
             "password": "pw",
             "name": "Test Testov",
         }
-        response = self.client.get(CREATE_USER_URL, payload)
+        response = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
