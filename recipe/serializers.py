f7de92b544d779eff0424fe2ba1e31a40fd55cf9
Bobur Yusupov <boburinfo2006@gmail.com>
2023-06-23 13:34:08 +0500

    Tags app completed with test

--- recipe/serializers.py
+++ recipe/serializers.py
@@ -3,7 +3,9 @@ Serializers for Recipe APIs
 """
 from rest_framework.serializers import ModelSerializer
 
-from core.models import Recipe, Tag
+from core.models import (
+    Recipe, 
+    Tag)
 
 class TagSerializer(ModelSerializer):
     """Serializer for tags."""
@@ -22,19 +24,38 @@ class RecipeSerializer(ModelSerializer):
         fields = ["id", "title", "time_minutes", "price", "link", "tags"]
         read_only_fields = ["id"]
     
-    def create(self, validated_data):
-        tags = validated_data.pop('tags', [])
-        recipe = Recipe.objects.create(**validated_data)
+    def _get_or_create_tags(self, tags, recipe):
+        """Handle getting pr creating tags as needed."""
         auth_user = self.context['request'].user
-
         for tag in tags:
             tag_obj, created = Tag.objects.get_or_create(
                 user=auth_user,
-                name=tag['name'],
+                **tag,
             )
             recipe.tags.add(tag_obj)
         
+
+    def create(self, validated_data):
+        tags = validated_data.pop('tags', [])
+        recipe = Recipe.objects.create(**validated_data)
+        self._get_or_create_tags(tags, recipe)
+
+        
         return recipe
+    
+    def update(self, instance, validated_data):
+        """Update recipe."""
+        tags = validated_data.pop('tags', None)
+
+        if tags is not None:
+            instance.tags.clear()
+            self._get_or_create_tags(tags, instance)
+        
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
 
 class RecipeDetailSerializer(RecipeSerializer):
     """Recipe detail serializers."""
